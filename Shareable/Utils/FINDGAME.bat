35c3b841fd471dc3ee52f8dbae0453bedbbf6b6c
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-05 18:05:00 -0400

    Add files via upload

--- Shareable/Utils/FINDGAME.bat
+++ Shareable/Utils/FINDGAME.bat
@@ -41,52 +41,62 @@ for %%i in ("%script_path%\..") do set "archive_path=%%~fi\archive\pc\mod"
 set "defaultSteamPath=%ProgramFiles(x86)%\Steam"
 set "libraryFoldersFile=%defaultSteamPath%\steamapps\libraryfolders.vdf"
 
-call :checkFolder "%defaultSteamPath%"
+call :checkSteamFolder "%defaultSteamPath%"
 if !errorlevel! == 0 goto :found
 
-if not exist "%libraryFoldersFile%" (
-    echo [ERROR] Cannot find Steam library...
-	echo [SUCCESS] You will need to manually drag the archive folder into /Cyberpunk 2077 game folder...
-    pause
-    exit /b 1
-)
-
-for /f "usebackq tokens=*" %%l in ("%libraryFoldersFile%") do (
-    set "line=%%l"
-	echo !line! | findstr /i "\"path\"" >nul
-    if !errorlevel! == 0 (
-        for /f "tokens=2 delims=	" %%a in ("!line!") do (
-            set "libPath=%%~a"
-            set "libPath=!libPath:"=!"
-            set "libPath=!libPath:\\=\!"
-            call :checkFolder "!libPath!"
-            if !errorlevel! == 0 goto :found
+if exist "%libraryFoldersFile%" (
+    for /f "usebackq tokens=*" %%l in ("%libraryFoldersFile%") do (
+        set "line=%%l"
+        echo !line! | findstr /i "\"path\"" >nul
+        if !errorlevel! == 0 (
+            for /f "tokens=2 delims=	" %%a in ("!line!") do (
+                set "libPath=%%~a"
+                set "libPath=!libPath:"=!"
+                set "libPath=!libPath:\\=\!"
+                call :checkSteamFolder "!libPath!"
+                if !errorlevel! == 0 goto :found
+            )
         )
     )
 )
 
+set "gogPath=%ProgramFiles(x86)%\GOG Galaxy\Games\Cyberpunk 2077"
+call :checkGOGFolder "!gogPath!"
+if !errorlevel! == 0 goto :found
 
+echo [ERROR] Could not find Cyberpunk 2077 install in Steam or GOG.
+echo [INFO] Please copy the mod files manually to your game folder.
+pause
 exit /b 1
 
-:checkFolder
+:checkSteamFolder
 set "gamePath=%~1\steamapps\common\Cyberpunk 2077"
 if exist "!gamePath!\bin\x64\Cyberpunk2077.exe" (
-
     set "mod_folder=!gamePath!\archive\pc\mod"
-
     if not exist "!mod_folder!" (
         mkdir "!mod_folder!"
     )
-
     xcopy /E /Y /I "%archive_path%\*" "!mod_folder!\" >nul
+    echo [INFO] Files copied to Steam path: !mod_folder!
+    exit /b 0
+)
+exit /b 1
 
-    echo [INFO] Files copied successfully to !mod_folder!
-	echo [SUCCESS] You can now launch the game.
+:checkGOGFolder
+set "gamePath=%~1"
+if exist "!gamePath!\bin\x64\Cyberpunk2077.exe" (
+    set "mod_folder=!gamePath!\archive\pc\mod"
+    if not exist "!mod_folder!" (
+        mkdir "!mod_folder!"
+    )
+    xcopy /E /Y /I "%archive_path%\*" "!mod_folder!\" >nul
+    echo [INFO] Files copied to GOG path: !mod_folder!
     exit /b 0
 )
 exit /b 1
 
 :found
+echo [SUCCESS] You can now launch the game.
 pause
 exit /b 0
 
