a22922bfdeab9c4ab139c29202491610fb5c7971
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-05 12:41:24 -0400

    Add files via upload

---
+++ Shareable/Utils/GETHELPERS.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "UTILS_DIR=Utils"
+set "VERSION_FILE=%UTILS_DIR%\version.txt"
+set "REMOTE_VERSION_URL=https://raw.githubusercontent.com/legitzombie/HelperBatch/main/Shareable/Utils/version.txt"
+
+if not exist "%UTILS_DIR%" mkdir "%UTILS_DIR%"
+
+set "TEMP_VERSION_FILE=%TEMP%\utils_version.tmp"
+curl -s -L -o "%TEMP_VERSION_FILE%" "%REMOTE_VERSION_URL%"
+if errorlevel 1 (
+    echo [WARN] Could not fetch remote version, update cancelled.
+    set "REMOTE_VERSION="
+) else (
+    set /p REMOTE_VERSION=<"%TEMP_VERSION_FILE%"
+)
+del "%TEMP_VERSION_FILE%"
+
+set "LOCAL_VERSION="
+if exist "%VERSION_FILE%" (
+    set /p LOCAL_VERSION=<"%VERSION_FILE%"
+)
+
+if "%REMOTE_VERSION%"=="%LOCAL_VERSION%" (
+    goto :done
+)
+
+echo Updating helper files...
+
+set "BASE_URL=https://raw.githubusercontent.com/legitzombie/HelperBatch/main/Shareable/Utils"
+
+set FILES=CLEANUP.bat FINDGAME.bat GET7ZIP.bat GETATLASGENERATOR.bat GETBINK.bat GETCLI.bat GETDOTNET.bat GETPILLOW.bat GETPIP.bat GETPYTHON.bat GETREDIST.bat GETREQUESTS.bat MP4TOBK2.bat
+
+for %%F in (%FILES%) do (
+    echo Downloading %%F...
+    curl -L -o "%UTILS_DIR%\%%F" "%BASE_URL%/%%F"
+)
+
+echo Helpers updated to version %REMOTE_VERSION%
+
+:done
+exit /b 0
