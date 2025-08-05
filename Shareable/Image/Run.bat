ab52add3e6778a5b91a5163485ed6aa50976902a
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-05 13:15:52 -0400

    Add files via upload

---
+++ Shareable/Image/Run.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal
+
+color 0D
+set "PATH=%SystemRoot%\System32;%PATH%"
+
+call Utils/GETHELPERS.bat
+
+call Utils/GETREDIST.bat
+
+call Utils/GETDOTNET.bat
+
+if %errorlevel%==123 exit
+
+call Utils/GETPYTHON.bat
+
+if %errorlevel%==123 exit
+
+call Utils/GETPIP.bat
+
+call Utils/GETPILLOW.bat
+
+call Utils/GETREQUESTS.bat
+
+call Utils/GETCLI.bat
+
+call Utils/CREATEFOLDERS.bat
+
+call Utils/RENAMEWALLPAPERS.bat
+
+call Utils/GETATLASGENERATOR.bat
+
+python.exe Utils/generate_inkatlas.py
+
+call Utils/JSON2INKATLAS.bat
+
+call Utils/PNG2XBM.bat 
+
+call Utils/ARCHIVE.bat
+
+call Utils/CLEANUP.bat
+
+call Utils/FINDGAME.bat
+
+endlocal
