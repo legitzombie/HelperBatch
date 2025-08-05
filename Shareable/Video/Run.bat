a3a4139d5c92964269e8ca939e6e6396fec259fe
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-05 13:10:25 -0400

    Add files via upload

---
+++ Shareable/Video/Run.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+color 0D
+set "PATH=%SystemRoot%\System32;%PATH%"
+
+call Utils/GETHELPERS.bat
+
+call Utils/GETREDIST.bat
+
+call Utils/GETDOTNET.bat
+
+if %errorlevel%==123 exit
+
+call Utils/GETCLI.bat
+
+call Utils/GET7ZIP.bat
+
+call Utils/GETBINK.bat
+
+call Utils/MP4TOBK2.bat
+
+call Utils/CREATEFOLDERS.bat
+
+call Utils/RENAMEVIDEO.bat
+
+call Utils/ARCHIVE.bat
+
+call Utils/CLEANUP.bat
+
+call Utils/FINDGAME.bat
+
+endlocal
