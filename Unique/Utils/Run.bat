ebfb8886942850ad294a47be09a55621b38863b4
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-05 12:41:55 -0400

    Add files via upload

--- Unique/Utils/Run.bat
+++ Unique/Utils/Run.bat
@@ -4,35 +4,25 @@ setlocal
 color 0D
 set "PATH=%SystemRoot%\System32;%PATH%"
 
+call Utils/GETHELPERS.bat
+
 call Utils/GETREDIST.bat
 
 call Utils/GETDOTNET.bat
 
 if %errorlevel%==123 exit
 
-call Utils/GETPYTHON.bat
-
-if %errorlevel%==123 exit
-
-call Utils/GETPIP.bat
-
-call Utils/GETPILLOW.bat
-
-call Utils/GETREQUESTS.bat
-
 call Utils/GETCLI.bat
+
+call Utils/GET7ZIP.bat
+
+call Utils/GETBINK.bat
+
+call Utils/MP4TOBK2.bat
 
 call Utils/CREATEFOLDERS.bat
 
-call Utils/RENAMEWALLPAPERS.bat
-
-call Utils/GETATLASGENERATOR.bat
-
-python.exe Utils/generate_inkatlas.py
-
-call Utils/JSON2INKATLAS.bat
-
-call Utils/PNG2XBM.bat 
+call Utils/RENAMEVIDEO.bat
 
 call Utils/ARCHIVE.bat
 
