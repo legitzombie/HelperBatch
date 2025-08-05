5ed9f5de3eba73a9b46b3c0829705ac4bc22623d
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-05 12:55:56 -0400

    Add files via upload

--- Shareable/Utils/GETHELPERS.bat
+++ Shareable/Utils/GETHELPERS.bat
@@ -4,6 +4,8 @@ setlocal enabledelayedexpansion
 set "UTILS_DIR=Utils"
 set "VERSION_FILE=%UTILS_DIR%\version.txt"
 set "REMOTE_VERSION_URL=https://raw.githubusercontent.com/legitzombie/HelperBatch/main/Shareable/Utils/version.txt"
+set "BASE_URL=https://raw.githubusercontent.com/legitzombie/HelperBatch/main/Shareable/Utils"
+set FILES=CLEANUP.bat FINDGAME.bat GET7ZIP.bat GETATLASGENERATOR.bat GETBINK.bat GETCLI.bat GETDOTNET.bat GETPILLOW.bat GETPIP.bat GETPYTHON.bat GETREDIST.bat GETREQUESTS.bat MP4TOBK2.bat
 
 if not exist "%UTILS_DIR%" mkdir "%UTILS_DIR%"
 
@@ -11,10 +13,9 @@ set "TEMP_VERSION_FILE=%TEMP%\utils_version.tmp"
 curl -s -L -o "%TEMP_VERSION_FILE%" "%REMOTE_VERSION_URL%"
 if errorlevel 1 (
     echo [WARN] Could not fetch remote version, update cancelled.
-    set "REMOTE_VERSION="
-) else (
-    set /p REMOTE_VERSION=<"%TEMP_VERSION_FILE%"
+    goto :done
 )
+set /p REMOTE_VERSION=<"%TEMP_VERSION_FILE%"
 del "%TEMP_VERSION_FILE%"
 
 set "LOCAL_VERSION="
@@ -23,21 +24,18 @@ if exist "%VERSION_FILE%" (
 )
 
 if "%REMOTE_VERSION%"=="%LOCAL_VERSION%" (
+    echo [INFO] Helpers are already up to date.
     goto :done
 )
 
-echo Updating helper files...
-
-set "BASE_URL=https://raw.githubusercontent.com/legitzombie/HelperBatch/main/Shareable/Utils"
-
-set FILES=CLEANUP.bat FINDGAME.bat GET7ZIP.bat GETATLASGENERATOR.bat GETBINK.bat GETCLI.bat GETDOTNET.bat GETPILLOW.bat GETPIP.bat GETPYTHON.bat GETREDIST.bat GETREQUESTS.bat MP4TOBK2.bat
-
+echo [INFO] Updating helper files to version %REMOTE_VERSION%...
 for %%F in (%FILES%) do (
     echo Downloading %%F...
-    curl -L -o "%UTILS_DIR%\%%F" "%BASE_URL%/%%F"
+    curl -s -L -o "%UTILS_DIR%\%%F" "%BASE_URL%/%%F"
 )
 
-echo Helpers updated to version %REMOTE_VERSION%
+echo %REMOTE_VERSION%>"%VERSION_FILE%"
+echo [INFO] Helpers updated to version %REMOTE_VERSION%.
 
 :done
 exit /b 0
