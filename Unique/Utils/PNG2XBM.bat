4e3820d98aa7c32696a4abc5ef98b93ab43dcc26
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-06 18:03:09 -0400

    Change path

--- Unique/Utils/PNG2XBM.bat
+++ Unique/Utils/PNG2XBM.bat
@@ -36,13 +36,14 @@ setlocal
 set "script_path=%~dp0"
 set "cli_path=%USERPROFILE%\.dotnet\tools\cp77tools.exe"
 
-for %%i in ("%script_path%\..") do set "image_path=%%~fi\temp\raw\base\"
+for %%i in ("%script_path%\..") do set "image_path=%%~fi\temp\raw\archive\base\"
 for %%i in ("%script_path%\..") do set "temp_path=%%~fi\temp\archive\base\"
 
 
 echo [INFO] Converting pngs to xbm... This can be a slow process...
 
 "%cli_path%" import -p "%image_path%" -o "%temp_path%"
+
 
 
 endlocal
