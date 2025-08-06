8a4e908b6d3ab47618981b266578025387d033b9
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-06 18:05:15 -0400

    Path Change & Dynamic filename

--- Unique/Utils/JSON2INKATLAS.bat
+++ Unique/Utils/JSON2INKATLAS.bat
@@ -36,20 +36,21 @@ setlocal
 set "script_path=%~dp0"
 set "cli_path=%USERPROFILE%\.dotnet\tools\cp77tools.exe"
 
-for %%i in ("%script_path%\..") do set "image_path=%%~fi\temp\raw\base\"
+for %%i in ("%script_path%\..") do set "image_path=%%~fi\temp\raw\archive\base\"
 for %%i in ("%script_path%\..") do set "temp_path=%%~fi\temp\archive\base\"
 
-set "json_file=%image_path%\wallpapers.inkatlas.json"
+set "json_file=%image_path%\custom.inkatlas.json"
 
 if exist "%json_file%" (
 
     %cli_path% convert d "%json_file%"
 
 	for %%F in ("%image_path%\*") do (
-		if /I "%%~nxF"=="wallpapers.inkatlas" (
+		if /I "%%~nxF"=="custom.inkatlas" (
 			move /Y "%%F" "%temp_path%\%%~nxF"
 		)
 	)
 )
 
 endlocal
+
