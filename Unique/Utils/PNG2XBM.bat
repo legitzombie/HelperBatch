07a85cc3e194840c0e1af11af2e0c0bc608fde56
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-07 15:58:32 -0400

    Update PNG2XBM.bat

--- Unique/Utils/PNG2XBM.bat
+++ Unique/Utils/PNG2XBM.bat
@@ -39,8 +39,11 @@ set "cli_path=%USERPROFILE%\.dotnet\tools\cp77tools.exe"
 for %%i in ("%script_path%\..") do set "image_path=%%~fi\temp\raw\base\"
 for %%i in ("%script_path%\..") do set "temp_path=%%~fi\temp\archive\base\"
 
-set "temp_path=%temp_path:\=/%"
 set "image_path=%image_path:\=/%"
+if "%image_path:~-1%"=="/" set "image_path=%image_path:~0,-1%"
+
+set "temp_path=%temp_path:\=/%"
+if "%temp_path:~-1%"=="/" set "temp_path=%temp_path:~0,-1%"
 
 echo [INFO] Converting pngs to xbm... This can be a slow process...
 
@@ -49,6 +52,7 @@ echo [INFO] Converting pngs to xbm... This can be a slow process...
 
 
 endlocal
+
 
 
 
