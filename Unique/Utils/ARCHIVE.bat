9a50d02fb78b7d9a5a6a8fc8a2ad2529e29b7050
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-07 14:54:24 -0400

    Update ARCHIVE.bat

--- Unique/Utils/ARCHIVE.bat
+++ Unique/Utils/ARCHIVE.bat
@@ -40,12 +40,11 @@ for %%i in ("%script_path%\..") do set "output_path=%%~fi"
 for %%i in ("%script_path%\..") do set "archive_path=%%~fi/temp/archive"
 for %%i in ("%script_path%\..") do set "perm_path=%%~fi/archive/pc/mod"
 
-
-for %%i in ("%script_path%\..") do set "wallpaper_path=%%~fi\temp\raw"
-for %%i in ("%script_path%\..") do set "temp_path=%%~fi\temp\archive\base\"
+for %%i in ("%script_path%\..") do set "temp_path=%%~fi/temp\archive\base\"
 
 "%cli_path%" pack -p "%archive_path%" -o "%perm_path%"
 
 rename "%perm_path%\archive.archive" "Custom.archive"
 
 endlocal
+
