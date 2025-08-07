e912dc34c284624fa0f42a94235c5447c0ef66e1
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-07 15:09:47 -0400

    Update CREATEFOLDERS.bat

--- Unique/Utils/CREATEFOLDERS.bat
+++ Unique/Utils/CREATEFOLDERS.bat
@@ -39,7 +39,7 @@ for %%i in ("%script_path%\..") do set "temp_path=%%~fi\temp"
 
 if not exist "%temp_path%" (
 	mkdir "%temp_path%"
-	mkdir "%temp_path%/archive/base/"
+	mkdir "%temp_path%\archive\base\"
 	mkdir "%temp_path%/raw/base"
 	mkdir "%temp_path%/images"
 	mkdir "%temp_path%/video"
@@ -56,6 +56,7 @@ mkdir "%perm_path%"
 
 
 endlocal
+
 
 
 
